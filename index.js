0aa6a17a96a80aeda95d522400fdd01e61075c01
0Apexx0 <adityaapexx@gmail.com>
2022-02-02 15:33:25 +0530

    Add directory to the project

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const port = 7000 ;
 const app = express();
 
+
+app.use('/', require('./routes'));
+
+
+// connecting to the server
 app.listen(port , (err)=>{
 
     if(err){console.log(`Error in connecting server : ${err}`); return;}
